2f3df5ecd09291198083981811a5b4ed031ee3fa
mikinakajima <miki.nakajima1202@gmail.com>
2020-04-06 20:46:04 -0400

    Added colormap downloader

---
+++ DownloadColormaps.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+curl -O http://www.fabiocrameri.ch/resources/lapaz.zip
+curl -O http://www.fabiocrameri.ch/resources/vik.zip
+curl -O http://www.fabiocrameri.ch/resources/turku.zip
+
+unzip lapaz.zip
+unzip vik.zip
+unzip turku.zip
+
