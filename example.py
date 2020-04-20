b70492b8fc9a6d85890a2cd674ce7d8627878d62
ScottHull <scottdhull21@gmail.com>
2020-04-20 17:04:15 -0400

    Initial commit.

---
+++ example.py
@@ -0,0 +1,5 @@
+from melt_model_scott_edit import Model
+
+m = Model(Mtotal=2.0, gamma=0.5, vel=2.0, entropy0=1100, outputfigurename="output.eps", use_tex=False)
+m.run_model()
+m.plot_model(save=False)
