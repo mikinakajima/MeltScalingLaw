572c4c3daa5be30c8428ce6dcf0e38e27dcc7773
ScottHull <scottdhull21@gmail.com>
2020-04-20 17:13:32 -0400

    Initial commit.

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from melt_model_scott_edit import Model
+from melt_model_Nakajima_et_al import Model
 
 m = Model(Mtotal=2.0, gamma=0.5, vel=2.0, entropy0=1100, outputfigurename="output.eps", use_tex=False)
 m.run_model()
