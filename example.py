cb400f3fa12c289a1aa49aa8a8150a0bbe216d7e
Miki <miki.nakajima1202@gmail.com>
2020-04-20 22:40:45 -0400

    update the melt model name

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from melt_model_Nakajima_et_al import Model
+from melt_model import Model
 
 m = Model(Mtotal=8.9, gamma=0.09, vel=1.0, entropy0=1100, impact_angle=90, outputfigurename="output.eps", use_tex=False)
 m.run_model()
