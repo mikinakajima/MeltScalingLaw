b06cb6d9e2a5d43ed73c8a2e61ad2cfeb7b08296
Miki <miki.nakajima1202@gmail.com>
2020-04-20 22:20:40 -0400

    update input parameters

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 from melt_model_Nakajima_et_al import Model
 
-m = Model(Mtotal=2.0, gamma=0.5, vel=2.0, entropy0=1100, outputfigurename="output.eps", use_tex=False)
+m = Model(Mtotal=8.9, gamma=0.09, vel=1.0, entropy0=1100, impact_angle=90, outputfigurename="output.eps", use_tex=False)
 m.run_model()
 m.plot_model(save=False)
