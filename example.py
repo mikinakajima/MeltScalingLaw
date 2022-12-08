e3fb290b3e727a83a3b7353dbdc41366bfe86163
Miki <miki.nakajima1202@gmail.com>
2022-12-08 14:02:23 -0500

    Update example.py

--- example.py
+++ example.py
@@ -3,3 +3,6 @@ from melt_model import Model
 m = Model(Mtotal=8.9, gamma=0.09, vel=1.0, entropy0=1100, impact_angle=90, outputfigurename="output.png", use_tex=False)
 resp = m.run_model()
 m.plot_model(save=True)
+
+# if you want to produce an output, for example impact velocity.
+# print(resp["impact velocity"])
