ae11782a9fb3ece2eac2b265598b7a78c7b730f2
Miki <miki.nakajima1202@gmail.com>
2026-06-17 14:59:36 -0400

    Update example.py

--- example.py
+++ example.py
@@ -6,10 +6,5 @@ m.plot_model(save=True)
 
 # if you want to produce an output, for example impact velocity, write
 print('fractional melt volume: ' + str(data["fractional melt volume"]))
-print('fractional melt volume (max): ' + str(data["fractional melt volume max"]))
-print('fractional melt volume (min): ' + str(data["fractional melt volume min"]))
 print('fractional melt mass: '         + str(data["fractional melt mass"]))
-print('fractional melt mass (max): '   + str(data["fractional melt mass max"]))
-print('fractional melt mass (min): '   + str(data["fractional melt mass min"]))
-print('total mantle mass: ' + str(data["total mantle mass"]))
-print('total mantle mass: ' + str(data["total volume"]))
+
