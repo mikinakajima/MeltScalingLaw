2a9d3e6848d0c00fab14ca6af29c38868002f5d9
Miki <miki.nakajima1202@gmail.com>
2026-06-17 15:03:34 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from melt_model import Model
 
-m = Model(Mtotal=8.9, gamma=0.09, vel=1.0, entropy0=1100, impact_angle=30, outputfigurename="output.png", use_tex=False)
+m = Model(Mtotal=8.9, gamma=0.09, vel=1.0, entropy0=1100, impact_angle=90, outputfigurename="output.png", use_tex=False)
 data = m.run_model()
 m.plot_model(save=True)
 
